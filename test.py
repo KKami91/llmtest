c835f0d8b92254952e693e0115a10001a26d5039
KKami91 <85279584+KKami91@users.noreply.github.com>
2024-04-05 14:01:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -49,11 +49,11 @@ def add_json_from_url_to_chroma_db(url, chroma_db):
         # Chroma DB에 JSON 데이터 추가
         chroma_db.add_data(json_data)
         
-        print(f"Successfully added data from {url}")
+    #    print(f"Successfully added data from {url}")
     except requests.RequestException as e:
-        print(f"Request failed: {e}")
+    #    print(f"Request failed: {e}")
     except Exception as e:
-        print(f"An error occurred while adding data to Chroma DB: {e}")
+    #    print(f"An error occurred while adding data to Chroma DB: {e}")
 
 # PDF 전문 파일을 Chroma DB에 추가하는 함수
 def pdf_to_document(uploaded_file):
@@ -61,7 +61,7 @@ def pdf_to_document(uploaded_file):
     temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
     with open(temp_filepath, "wb") as f:
         f.write(uploaded_file.getvalue())
-    print(f'{getSTime()} :Upload complete -- {uploaded_file}')
+    #print(f'{getSTime()} :Upload complete -- {uploaded_file}')
     loader = PyPDFLoader(temp_filepath)
     pages = loader.load_and_split()
 
@@ -70,41 +70,40 @@ def pdf_to_document(uploaded_file):
 # 업로드 되면 동작하는 코드
 if uploaded_file is not None:
     pages = pdf_to_document(uploaded_file)
-    print(f'{getSTime()} :PDF processing complete -- {uploaded_file}')
+    #print(f'{getSTime()} :PDF processing complete -- {uploaded_file}')
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 300, chunk_overlap=20, length_function = len, is_separator_regex= False)
     embedded_model = OpenAIEmbeddings()
 
     texts = text_splitter.split_documents(pages)
-    print(texts)
+    #print(texts)
     db = Chroma.from_documents(texts, embedded_model)
 
    
 
     texts = text_splitter.split_documents(pages)
-'''
+
     db = Chroma.from_documents(texts, embedded_model)
-    print(f'{getSTime()} :Vector DB Load complete -- {db}')
+    #print(f'{getSTime()} :Vector DB Load complete -- {db}')
 
     st.header("정신건강 AI에게 문의")
     question = st.text_input("문의 내용을 입력하세요")
 
     if st.button('문의하기'):
 
-        print(f'{getSTime()} :Question -- {question}')
+    #    print(f'{getSTime()} :Question -- {question}')
         llm = ChatOpenAI(model_name="gpt-4", temperature=0)
-        qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+        qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever()) 
        
         result = qa_chain({"query": question})
-        print(f'{getSTime()} :Retriever send complete --')
+    #    print(f'{getSTime()} :Retriever send complete --')
         
         st.write(result)
         
         print(result)
-        print("----- embedding ----")
+    #    print("----- embedding ----")
         retriever_from_llm = MultiQueryRetriever.from_llm(retriever=db.as_retriever(), llm=llm)
         docs = retriever_from_llm.get_relevant_documents(query=question)
         
         
-        print(f'{getSTime()} :Result {question} --> {docs}')
+    #    print(f'{getSTime()} :Result {question} --> {docs}')
    #llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-'''
